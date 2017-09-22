cb6b78ee959d0d468bf722d160ed558ecd9e741b
hisham <hesham.osman28@yahoo.com>
2017-09-22 12:33:19 +0200

    Complaint Listing Ejb facade

--- TheContributor-ejb/src/java/com/sypron/facade/ComplaintFacade.java
+++ TheContributor-ejb/src/java/com/sypron/facade/ComplaintFacade.java
@@ -6,9 +6,13 @@
 package com.sypron.facade;
 
 import com.sypron.entity.Complaint;
+import com.sypron.entity.Permission;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -27,6 +31,28 @@ public class ComplaintFacade extends AbstractFacade<Complaint> {
 
     public ComplaintFacade() {
         super(Complaint.class);
+    }
+    
+    public List<Complaint> getComplaintsByScope(String scope,Integer currentUserId,Integer departmentId){
+        TypedQuery<Complaint> query = null;
+        switch(scope){
+            case "user":
+                query = em.createQuery("SELECT c From Complaint c WHERE c.user.id = :userId ",
+                     Complaint.class).setParameter("userId", currentUserId);
+                break;
+            case "department":
+                query = em.createQuery("SELECT c From Complaint c WHERE c.user.departmentRole.department.id = :userId ",
+                     Complaint.class).setParameter("userId", currentUserId);
+                break;    
+            case "company":
+                query = em.createQuery("SELECT c From Complaint c ",
+                     Complaint.class);
+                break;        
+        }
+        if(query == null)
+            return null;
+        else  
+            return query.getResultList();
     }
     
 }
